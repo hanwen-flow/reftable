e4e34a99017e702dcea1c0dbd2dc7f8ff7c70bb0
Han-Wen Nienhuys <hanwen@google.com>
2021-09-09 20:23:58 +0200

    C: move uncompress2 declaration to system.h
    Helped-by: Carlo Marcelo Arenas Belón <carenas@gmail.com>

--- c/block.c
+++ c/block.c
@@ -15,15 +15,6 @@ https://developers.google.com/open-source/licenses/bsd
 #include "system.h"
 #include <zlib.h>
 
-#ifdef NO_UNCOMPRESS2
-/*
- * This is uncompress2, which is only available in zlib >= 1.2.9
- * (released as of early 2017)
- */
-int uncompress2(Bytef *dest, uLongf *destLen, const Bytef *source,
-		uLong *sourceLen);
-#endif
-
 int header_size(int version)
 {
 	switch (version) {
